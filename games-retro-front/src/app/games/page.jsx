36da57cb7c9539bcefeaffbbcd0126466c118e88
jessicaprestelo1201 <jessica.p.jesus@aluno.senai.br>
2025-05-15 08:42:18 -0300

    feat: create games page

---
+++ games-retro-front/src/app/games/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.games}>
+            <h1>Página de Games</h1>
+        </div>
+    );
+}
--- games-retro-front/src/app/page.jsx
+++ games-retro-front/src/app/page.jsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Recordes de jogo</h1>
+      <h1>Games Retrô</h1>
+      <p>Record de jogos retrô</p>
     </main>
   );
 }
